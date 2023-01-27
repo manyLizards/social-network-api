c676f1e3d3a1055eb2481e727b1074d216221a40
Jordan <jnbarringer96@gmail.com>
2023-01-27 13:49:45 -0600

    fleshed out User model

--- Models/User.js
+++ Models/User.js
@@ -2,8 +2,17 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var userSchema = new Schema({
-    username: String, required: true, //trim it
-    email: String, required: true, //make it unique AND match 
+    username: {
+        type: String, 
+        required: true, 
+        trim: true,
+    },
+    email: {
+        type: String, 
+        required: true, 
+        unique: true, 
+        validate: [ isEmail, 'invalid email']
+    },
     thoughts: //reference thought model array of id values
     ,
     friends: //reference array of id values from this model
