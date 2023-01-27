186bacba9e23f4415b0c6b320f2b2772aea72fe2
Jordan <jnbarringer96@gmail.com>
2023-01-27 13:11:35 -0600

    added all instructions

---
+++ Controller/API/Users.js
@@ -0,0 +1,9 @@
+//Get all users
+//Get a single user by its _id and populated thought and friend data
+//POST a new User
+//Example data {"username": "lernantino", "email": "lernantino@gmail.com"}
+//PUT to update a user by its _id
+//DELETE to remove user by its _id
+//Remove a user's associated thoughts when deleted
+//:userId/friends/:friendId -> POST to add a new friend to a user's friend list
+// DELETE to remove a friend from a user's friend list
